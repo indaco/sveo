93dad9408484f302aa40ccdf2dc34da11872dc8c
indaco <indaco@users.noreply.github.com>
2025-04-04 09:22:01 +0200

    refactor(metadata/opengraph): remove unused re-exports, keep only OpenGraph

--- src/lib/components/metadata/opengraph/index.ts
+++ src/lib/components/metadata/opengraph/index.ts
@@ -1,12 +1,1 @@
 export { default as OpenGraph } from './OpenGraph.svelte';
-export { default as OpenGraphArticle } from './article.svelte';
-export { default as OpenGraphBook } from './book.svelte';
-export { default as OpenGraphBusiness } from './business.svelte';
-export { default as OpenGraphMusicAlbum } from './music-album.svelte';
-export { default as OpenGraphMusicPlaylist } from './music-playlist.svelte';
-export { default as OpenGraphMusicRadioStation } from './music-radio-station.svelte';
-export { default as OpenGraphMusicSong } from './music-song.svelte';
-export { default as OpenGraphProduct } from './product.svelte';
-export { default as OpenGraphProfile } from './profile.svelte';
-export { default as OpenGraphVideoEpisode } from './video-episode.svelte';
-export { default as OpenGraphVideoMovie } from './video-movie.svelte';
