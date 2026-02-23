9424862009088bc70034d71a7bf8e9bafc8b40dd
indaco <indaco@users.noreply.github.com>
2026-02-23 16:05:12 +0100

    fix: use name attribute for twitter meta tags
    Twitter cards use name= not property= for meta tags per the spec.
    Update all occurrences and fix querySelector in page test.

--- src/lib/components/metadata/twittercard/TwitterCard.svelte
+++ src/lib/components/metadata/twittercard/TwitterCard.svelte
@@ -11,34 +11,34 @@
 
 {#if data.twitter}
 	{#if data.twitter.type}
-		<meta property="twitter:card" content={data.twitter.type} />
-		<meta property="twitter:title" content={data.title} />
+		<meta name="twitter:card" content={data.twitter.type} />
+		<meta name="twitter:title" content={data.title} />
 		{#if data.twitter.site}
-			<meta property="twitter:site" content={data.twitter.site} />
+			<meta name="twitter:site" content={data.twitter.site} />
 		{/if}
 		{#if data.description}
-			<meta property="twitter:description" content={data.description} />
+			<meta name="twitter:description" content={data.description} />
 		{/if}
 		{#if data.image?.url}
-			<meta property="twitter:image" content={data.image.url} />
+			<meta name="twitter:image" content={data.image.url} />
 			{#if data.image.alt}
-				<meta property="twitter:image:alt" content={data.image.alt} />
+				<meta name="twitter:image:alt" content={data.image.alt} />
 			{/if}
 		{/if}
 
 		{#if data.twitter.type === TwitterCardType.Player && data.twitter.player}
-			<meta property="twitter:player" content={data.twitter.player.url} />
-			<meta property="twitter:player:width" content={data.twitter.player.width.toString()} />
-			<meta property="twitter:player:height" content={data.twitter.player.height.toString()} />
+			<meta name="twitter:player" content={data.twitter.player.url} />
+			<meta name="twitter:player:width" content={data.twitter.player.width.toString()} />
+			<meta name="twitter:player:height" content={data.twitter.player.height.toString()} />
 		{/if}
 
 		{#if data.twitter.type === TwitterCardType.App && data.twitter.app}
 			{#if data.twitter.app.country}
-				<meta property="twitter:app:country" content={data.twitter.app.country} />
+				<meta name="twitter:app:country" content={data.twitter.app.country} />
 			{/if}
-			<meta property="twitter:app:id:iphone" content={data.twitter.app.idIPhone} />
-			<meta property="twitter:app:id:ipad" content={data.twitter.app.idIPad} />
-			<meta property="twitter:app:id:googleplay" content={data.twitter.app.idGooglePlay} />
+			<meta name="twitter:app:id:iphone" content={data.twitter.app.idIPhone} />
+			<meta name="twitter:app:id:ipad" content={data.twitter.app.idIPad} />
+			<meta name="twitter:app:id:googleplay" content={data.twitter.app.idGooglePlay} />
 		{/if}
 	{/if}
 {/if}
